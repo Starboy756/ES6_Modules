21a378243d5d9ce4190c87d0be8f800177915582
Starboy756 <taseentariq5@gmail.com>
2025-08-07 19:53:33 +0500

    added new things

---
+++ script.js
@@ -0,0 +1,7 @@
+import {PI , getCircumference ,getArea} from './mathUtils.js';
+
+console.log(getCircumference(5).toFixed(2));
+console.log(getArea(5).toFixed(2));
+
+const Pi = PI;
+console.log(Pi);
